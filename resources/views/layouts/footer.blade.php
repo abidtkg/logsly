78cfa105076b4de2e678f5aa8680f2914bcf4d06
Abid Hasan <abidtkg@gmail.com>
2024-01-17 19:04:57 +0600

    Update theme

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            , made with ❤️ by
+            <span class="fw-bolder">{{ config('app.name', 'Laravel') }}</span>
+        </div>
+        <div>
+
+            <a href="https://bdassistant.com" target="_blank" class="footer-link me-4">BD Assistant Ltd.</a>
+        </div>
+    </div>
+</footer>
